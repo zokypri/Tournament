365038de3b83dc3fdfb6ab11a4ce2594ce019306
zpri <zoky.zp@gmail.com>
2024-03-26 19:46:26 +0100

    printing the swagger URL

--- src/main/kotlin/com/paf/exercise/exercise/ExerciseApplication.kt
+++ src/main/kotlin/com/paf/exercise/exercise/ExerciseApplication.kt
@@ -8,4 +8,7 @@ class ExerciseApplication
 
 fun main(args: Array<String>) {
 	runApplication<ExerciseApplication>(*args)
+
+	val swaggerUrl = "/exercise/swagger-ui/index.html#/";
+	println("Swagger URL: http://localhost:8082$swaggerUrl")
 }
